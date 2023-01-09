41a296d4ec0ce7c96a3f38fdf29af32c5b6dc899
Timothy Small <smalls.online@icloud.com>
2023-01-09 11:52:51 -0500

    Added footnote fixup logic
    Since Markdig adds links directly to footnotes, we need to be able to rewrite the links it generates to the current page. If this isn't done, the link it generates is to the home route.

--- src/SmallsOnline.Web.Lib.Components/components/blog/BlogEntryCard.razor.cs
+++ src/SmallsOnline.Web.Lib.Components/components/blog/BlogEntryCard.razor.cs
@@ -1,9 +1,76 @@
+using System.Text.RegularExpressions;
 using SmallsOnline.Web.Lib.Models.Blog;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.Extensions.Logging;
 
 namespace SmallsOnline.Web.Lib.Components.Blog;
 
 public partial class BlogEntryCard : ComponentBase
 {
+    [Inject]
+    protected ILogger<BlogEntryCard> ComponentLogger { get; set; } = null!;
+
     [Parameter, EditorRequired]
     public BlogEntry BlogEntry { get; set; } = null!;
+
+    [Parameter]
+    public NavigationManager? NavigationManager { get; set; }
+
+    private string? _contentHtml;
+
+    protected override void OnParametersSet()
+    {
+        if (NavigationManager is not null)
+        {
+            _contentHtml = FixFootnoteLinks();
+        }
+        else
+        {
+            _contentHtml = BlogEntry.ContentHtml;
+        }
+
+        base.OnParametersSet();
+    }
+
+    private string FixFootnoteLinks()
+    {
+        if (BlogEntry is not null && BlogEntry.ContentHtml is not null)
+        {
+            Regex footnoteLinkRegex =
+                new(
+                    "<a (?'idProperty'id=\"(?'id'(?:fnref|fn):.+?)\" |)href=\"(?'footnoteAnchorTag'#(?:fnref|fn):.+?\") class=\"(?'class'.+?)\">");
+
+            string modifiedEntryContent = BlogEntry.ContentHtml;
+
+            Match footnoteLinkMatch = footnoteLinkRegex.Match(BlogEntry.ContentHtml);
+
+            while (footnoteLinkMatch.Success)
+            {
+                ComponentLogger.LogInformation("Fixing footnote link: '{text}'", footnoteLinkMatch.Value);
+                if (footnoteLinkMatch.Groups["idProperty"] is not null &&
+                    footnoteLinkMatch.Groups["footnoteAnchorTag"] is not null)
+                {
+                    modifiedEntryContent = modifiedEntryContent.Replace(
+                        oldValue: footnoteLinkMatch.Value,
+                        newValue:
+                        $"<a id=\"{footnoteLinkMatch.Groups["id"].Value}\" href=\"{NavigationManager.Uri}{footnoteLinkMatch.Groups["footnoteAnchorTag"].Value}\" class=\"{footnoteLinkMatch.Groups["class"].Value}\">"
+                    );
+                }
+                else
+                {
+                    modifiedEntryContent = modifiedEntryContent.Replace(
+                        oldValue: footnoteLinkMatch.Value,
+                        newValue:
+                        $"<a href=\"{NavigationManager.Uri}{footnoteLinkMatch.Groups["footnoteAnchorTag"].Value}\" class=\"{footnoteLinkMatch.Groups["class"].Value}\">"
+                    );
+                }
+
+                footnoteLinkMatch = footnoteLinkMatch.NextMatch();
+            }
+
+            return modifiedEntryContent;
+        }
+
+        throw new NullReferenceException("Content was null.");
+    }
 }
