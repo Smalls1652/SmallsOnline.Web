7ccf78229b42db17e63c0992661e407f34f8773e
Timothy Small <smalls.online@icloud.com>
2023-08-26 16:31:37 -0400

    Update FavoritesOf
    - Use the server for the database calls. No need for calling an API anymore.

--- src/PublicSite/Server/Pages/FavoritesOf.razor.cs
+++ src/PublicSite/Server/Pages/FavoritesOf.razor.cs
@@ -3,28 +3,21 @@ using System.Text.Json.Serialization.Metadata;
 using Microsoft.AspNetCore.Components.Routing;
 using SmallsOnline.Web.Lib.Models.FavoritesOf.Albums;
 using SmallsOnline.Web.Lib.Models.FavoritesOf.Songs;
+using SmallsOnline.Web.Lib.Services;
 
-namespace SmallsOnline.Web.PublicSite.Server;
+namespace SmallsOnline.Web.PublicSite.Server.Pages;
 
 /// <summary>
 /// The page for displaying the favorite music of a given year.
 /// </summary>
-public partial class FavoritesOf : ComponentBase, IDisposable
+[StreamRendering(false)]
+public partial class FavoritesOf : ComponentBase
 {
     [Inject]
-    protected IHttpClientFactory HttpClientFactory { get; set; } = null!;
-
-    [Inject]
-    protected PersistentComponentState AppState { get; set; } = null!;
+    protected ICosmosDbService CosmosDbService { get; set; } = null!;
 
     [Inject]
     protected ILogger<FavoritesOf> PageLogger { get; set; } = null!;
-
-    [Inject]
-    protected NavigationManager NavigationManager { get; set; } = null!;
-
-    [Inject]
-    protected FavoritesOfStateContainer StateContainer { get; set; } = null!;
 
     /// <summary>
     /// The list year to display data for.
@@ -33,53 +26,24 @@ public partial class FavoritesOf : ComponentBase, IDisposable
     public string? ListYear { get; set; }
 
     private bool _isFinishedLoading = false;
-    private PersistingComponentStateSubscription? _persistenceSubscription;
+
     private AlbumData[]? _albumItems;
     private SongData[]? _trackItems;
 
     private AlbumData? _bestAlbum;
     private AlbumData[]? _favoriteAlbums;
 
-    private ElementReference _favoriteAlbumsRef;
-    private ElementReference _favoriteSongsRef;
-
-
-    public void Dispose()
+    protected override async Task OnInitializedAsync()
     {
-        Dispose(true);
-        GC.SuppressFinalize(this);
-    }
-
-    protected override async Task OnParametersSetAsync()
-    {
-        StateContainer.OnChange += StateHasChanged;
-        NavigationManager.LocationChanged += OnLocationChange;
-
         if (ListYear is not null)
         {
-            _persistenceSubscription = AppState.RegisterOnPersisting(PersistFavoritesOfData);
-
             _isFinishedLoading = false;
 
-            if (ListYear != StateContainer.ListYear)
-            {
-                await GetFavorites();
-                StateContainer.ListYear = ListYear;
-            }
+            await GetFavorites();
 
             _isFinishedLoading = true;
-        }
-    }
 
-    private void OnLocationChange(object? sender, LocationChangedEventArgs eventArgs)
-    {
-        if (eventArgs.Location.Contains("/top-music/favorites-of/"))
-        {
-            StateContainer.ListYear = ListYear;
-        }
-        else
-        {
-            StateContainer.ListYear = null;
+            PageLogger.LogInformation("Finished loading favorites of {ListYear}", ListYear);
         }
     }
 
@@ -90,63 +54,13 @@ public partial class FavoritesOf : ComponentBase, IDisposable
     {
         PageLogger.LogInformation("Loading favorites of {ListYear}", ListYear);
 
-        bool isAlbumItemsDataAvailable = AppState.TryTakeFromJson(
-            key: $"albumItemsData-{ListYear}",
-            instance: out AlbumData[]? restoredAlbumItemsData
-        );
-
-        bool isTrackItemsDataAvailable = AppState.TryTakeFromJson(
-            key: $"trackItemsData-{ListYear}",
-            instance: out SongData[]? restoredTrackItemsData
-        );
-
-        using HttpClient httpClient = HttpClientFactory.CreateClient("PublicApi");
-        if (!isAlbumItemsDataAvailable)
-        {
-            // Get the favorite albums from the API.
-            _albumItems = await httpClient.GetFromJsonAsync(
-                requestUri: $"api/favorites-of/albums/{ListYear}",
-                jsonTypeInfo: _jsonSourceGenerationContext.AlbumDataArray
-            );
-        }
-        else
-        {
-            PageLogger.LogInformation(
-                "Album list data was persisted from a prerendered state. Restoring that data instead.");
-            _albumItems = restoredAlbumItemsData;
-        }
+        // Get the favorite albums from the API.
+        _albumItems = await CosmosDbService.GetFavoriteAlbumsOfYearAsync(ListYear!);
 
         FilterFavoriteAlbumsOfYear(_albumItems);
 
-        if (!isTrackItemsDataAvailable)
-        {
-            // Get the favorite tracks from the API.
-            _trackItems = await httpClient.GetFromJsonAsync(
-                requestUri: $"api/favorites-of/songs/{ListYear}",
-                jsonTypeInfo: _jsonSourceGenerationContext.SongDataArray
-            );
-        }
-        else
-        {
-            PageLogger.LogInformation(
-                "Track list data was persisted from a prerendered state. Restoring that data instead.");
-            _trackItems = restoredTrackItemsData;
-        }
-    }
-
-    private Task PersistFavoritesOfData()
-    {
-        AppState.PersistAsJson(
-            key: $"albumItemsData-{ListYear}",
-            instance: _albumItems
-        );
-
-        AppState.PersistAsJson(
-            key: $"trackItemsData-{ListYear}",
-            instance: _trackItems
-        );
-
-        return Task.CompletedTask;
+        // Get the favorite tracks from the API.
+        _trackItems = await CosmosDbService.GetFavoriteSongsOfYearAsync(ListYear!);
     }
 
     private void FilterFavoriteAlbumsOfYear(AlbumData[]? inputAlbums)
@@ -162,30 +76,6 @@ public partial class FavoritesOf : ComponentBase, IDisposable
                 array: inputAlbums,
                 match: (AlbumData item) => !item.IsBest
             );
-        }
-    }
-
-    private async Task ScrollToFavoriteAlbums()
-    {
-        await _favoriteAlbumsRef.FocusAsync();
-    }
-
-    private async Task ScrollToFavoriteTracks()
-    {
-        await _favoriteSongsRef.FocusAsync();
-    }
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (disposing)
-        {
-            if (_persistenceSubscription.HasValue)
-            {
-                _persistenceSubscription.Value.Dispose();
-            }
-
-            NavigationManager.LocationChanged -= OnLocationChange;
-            StateContainer.OnChange -= StateHasChanged;
         }
     }
 }
