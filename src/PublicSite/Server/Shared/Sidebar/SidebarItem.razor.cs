f2246f327991314226eef283d997da7e59bf45b9
Timothy Small <smalls.online@icloud.com>
2023-08-29 12:25:50 -0400

    Update SidebarItem
    - Switch to the WASM version of the profile image.
    - Pass boolean for whether logging should be enabled or not, based on if the site is in a development environment.

--- src/PublicSite/Server/Shared/Sidebar/SidebarItem.razor.cs
+++ src/PublicSite/Server/Shared/Sidebar/SidebarItem.razor.cs
@@ -5,5 +5,15 @@ namespace SmallsOnline.Web.PublicSite.Server.Shared.Sidebar;
 /// </summary>
 public partial class SidebarItem : ComponentBase
 {
+    [Inject]
+    protected IConfiguration Configuration { get; set; } = null!;
+
     private readonly string _mastodonUrl = "https://ocw.social/@smalls";
+
+    private bool _isDevelopmentMode;
+
+    protected override void OnInitialized()
+    {
+        _isDevelopmentMode = Configuration.GetValue<string>("ASPNETCORE_ENVIRONMENT") == "Development";
+    }
 }
