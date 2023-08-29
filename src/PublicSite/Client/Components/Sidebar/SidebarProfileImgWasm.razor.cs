4365fbdf3b2dae08b63d28aee911d6c74c97148b
Timothy Small <smalls.online@icloud.com>
2023-08-29 12:26:12 -0400

    Set SidebarProfileImgWasm to RenderModeAuto

--- src/PublicSite/Client/Components/Sidebar/SidebarProfileImgWasm.razor.cs
+++ src/PublicSite/Client/Components/Sidebar/SidebarProfileImgWasm.razor.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace SmallsOnline.Web.PublicSite.Client.Components.Sidebar;
 
+[RenderModeAuto(true)]
 public partial class SidebarProfileImgWasm : SidebarProfileImg
 {}
