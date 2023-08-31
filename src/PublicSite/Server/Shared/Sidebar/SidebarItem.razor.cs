73c4428f0d9f7f695a482b00db5a6e75dca9ec59
Timothy Small <smalls.online@icloud.com>
2023-08-31 13:37:10 -0400

    Update social links for sidebar
    - Each icon has text.
    - Each link is generated from an array.
    - Added borders between each link.

--- src/PublicSite/Server/Shared/Sidebar/SidebarItem.razor.cs
+++ src/PublicSite/Server/Shared/Sidebar/SidebarItem.razor.cs
@@ -8,6 +8,13 @@ public partial class SidebarItem : ComponentBase
     [Inject]
     protected IConfiguration Configuration { get; set; } = null!;
 
+    private SocialMediaItem[] _socialMediaItems = new SocialMediaItem[]
+    {
+        new("mastodon", "Mastodon", "https://ocw.social/@smalls", true),
+        new("github", "GitHub", "https://github.com/Smalls1652", false),
+        new("linkedin", "LinkedIn", "https://www.linkedin.com/in/timothy-small-a56965169", false)
+    };
+
     private readonly string _mastodonUrl = "https://ocw.social/@smalls";
 
     private bool _isDevelopmentMode;
