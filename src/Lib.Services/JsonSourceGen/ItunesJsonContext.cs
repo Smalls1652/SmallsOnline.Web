a2b31b56e5af465915e359e4f417c70da643db66
Timothy Small <smalls.online@icloud.com>
2023-10-02 09:23:52 -0400

    Add source generated JSON contexts for iTunes and Odesli

---
+++ src/Lib.Services/JsonSourceGen/ItunesJsonContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+using SmallsOnline.Web.Lib.Models.Itunes;
+
+namespace SmallsOnline.Web.Lib.Services;
+
+[JsonSourceGenerationOptions(
+    WriteIndented = false,
+    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+    GenerationMode = JsonSourceGenerationMode.Default,
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+)]
+[JsonSerializable(typeof(ArtistItem))]
+[JsonSerializable(typeof(ApiSearchResult<ArtistItem>))]
+[JsonSerializable(typeof(SongItem))]
+[JsonSerializable(typeof(ApiSearchResult<SongItem>))]
+[JsonSerializable(typeof(AlbumItem))]
+[JsonSerializable(typeof(ApiSearchResult<AlbumItem>))]
+internal partial class ItunesJsonContext : JsonSerializerContext
+{
+}
