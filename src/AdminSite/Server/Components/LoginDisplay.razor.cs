641cc40c3a5049041903c7b5904e73ec7c474cc9
Timothy Small <smalls.online@icloud.com>
2023-10-08 20:10:48 -0400

    Add LoginDisplay

---
+++ src/AdminSite/Server/Components/LoginDisplay.razor.cs
@@ -0,0 +1,8 @@
+namespace SmallsOnline.Web.AdminSite.Server.Shared;
+
+/// <summary>
+/// Component for displaying the login status.
+/// </summary>
+public partial class LoginDisplay : ComponentBase
+{
+}
