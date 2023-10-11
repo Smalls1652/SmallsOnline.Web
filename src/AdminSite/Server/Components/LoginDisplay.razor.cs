1829a543a5865bf9b60798974f5e856e7ab8b4c6
Timothy Small <smalls.online@icloud.com>
2023-10-11 09:46:07 -0400

    Move sign-in/out to NavbarItem
    Also make Favorite Music Of link only appear for logged in users.

--- src/AdminSite/Server/Components/LoginDisplay.razor.cs
+++
@@ -1,8 +0,0 @@
-namespace SmallsOnline.Web.AdminSite.Server.Shared;
-
-/// <summary>
-/// Component for displaying the login status.
-/// </summary>
-public partial class LoginDisplay : ComponentBase
-{
-}
