41244ccedc7318a07892569e6015120ac351b831
Timothy Small <smalls.online@icloud.com>
2023-10-13 11:28:02 -0400

    Add AdminSite.Client
    This is for _potentially_ using Server and WebAssembly modes. Right now, it's not being used.

---
+++ src/AdminSite/Client/JsonSourceGen/CoreJsonContext.cs
@@ -0,0 +1,48 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
+using SmallsOnline.Web.Lib.Models.AboutMe;
+using SmallsOnline.Web.Lib.Models.ActivityPub;
+using SmallsOnline.Web.Lib.Models.Blog;
+using SmallsOnline.Web.Lib.Models.Database;
+using SmallsOnline.Web.Lib.Models.FavoritesOf.Albums;
+using SmallsOnline.Web.Lib.Models.FavoritesOf.Songs;
+using SmallsOnline.Web.Lib.Models.MusicPlaylists;
+using SmallsOnline.Web.Lib.Models.Projects;
+
+namespace SmallsOnline.Web.AdminSite.Client.JsonSourceGen;
+
+[JsonSourceGenerationOptions(
+    WriteIndented = false,
+    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+    GenerationMode = JsonSourceGenerationMode.Default,
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+)]
+[JsonSerializable(typeof(SkillItems))]
+[JsonSerializable(typeof(SkillValue))]
+[JsonSerializable(typeof(WebFingerLink))]
+[JsonSerializable(typeof(WebFingerResponse))]
+[JsonSerializable(typeof(BlogEntries))]
+[JsonSerializable(typeof(BlogEntry))]
+[JsonSerializable(typeof(DatabaseItem))]
+[JsonSerializable(typeof(AlbumData))]
+[JsonSerializable(typeof(AlbumData[]))]
+[JsonSerializable(typeof(AlbumStandoutSong))]
+[JsonSerializable(typeof(AlbumStandoutSongItem))]
+[JsonSerializable(typeof(SongData))]
+[JsonSerializable(typeof(SongData[]))]
+[JsonSerializable(typeof(ProjectItem))]
+[JsonSerializable(typeof(ProjectItem[]))]
+[JsonSerializable(typeof(ProjectType))]
+[JsonSerializable(typeof(List<ProjectType>))]
+[JsonSerializable(typeof(Playlist))]
+[JsonSerializable(typeof(Playlist[]))]
+[JsonSerializable(typeof(PlaylistSong))]
+[JsonSerializable(typeof(PlaylistSong[]))]
+[JsonSerializable(typeof(SongShiftPlaylistExport))]
+[JsonSerializable(typeof(SongShiftPlaylistExport[]))]
+[JsonSerializable(typeof(SongShiftSong))]
+[JsonSerializable(typeof(SongShiftSong[]))]
+internal partial class CoreJsonContext : JsonSerializerContext
+{
+}
