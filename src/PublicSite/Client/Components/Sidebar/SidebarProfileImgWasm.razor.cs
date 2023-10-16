79dd15a0629fb0355ad748bbbdd9151babdc4ad2
Timothy Small <smalls.online@icloud.com>
2023-10-16 14:31:02 -0400

    Add XML docs to PublicSite.Client

--- src/PublicSite/Client/Components/Sidebar/SidebarProfileImgWasm.razor.cs
+++ src/PublicSite/Client/Components/Sidebar/SidebarProfileImgWasm.razor.cs
@@ -3,5 +3,8 @@ using Microsoft.AspNetCore.Components.Web;
 
 namespace SmallsOnline.Web.PublicSite.Client.Components.Sidebar;
 
+/// <summary>
+/// WebAssembly (WASM) implementation of <see cref="SidebarProfileImg"/>.
+/// </summary>
 public partial class SidebarProfileImgWasm : SidebarProfileImg
 {}
