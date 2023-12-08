48353c26610266640bd483f91cf0279b82a74000
Timothy Small <smalls.online@icloud.com>
2023-12-08 16:55:45 -0500

    Add UserAgent option

--- src/Lib.Services/Services/OdesliService/OdesliServiceOptions.cs
+++ src/Lib.Services/Services/OdesliService/OdesliServiceOptions.cs
@@ -3,6 +3,11 @@ namespace SmallsOnline.Web.Lib.Services;
 public class OdesliServiceOptions
 {
     /// <summary>
+    /// The user agent to use when making HTTP requests.
+    /// </summary>
+    public string UserAgent { get; set; } = null!;
+
+    /// <summary>
     /// Whether or not to enable logging.
     /// </summary>
     public bool EnableLogging { get; set; } = true;
