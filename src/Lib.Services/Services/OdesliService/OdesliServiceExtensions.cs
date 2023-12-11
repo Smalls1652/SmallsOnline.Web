a5be0e6cadc8900b45db1768c74c140cdfad24c8
Timothy Small <smalls.online@icloud.com>
2023-12-11 11:07:48 -0500

    Add extension method for OdesliService

--- src/Lib.Services/Services/OdesliService/OdesliServiceExtensions.cs
+++ src/Lib.Services/Services/OdesliService/OdesliServiceExtensions.cs
@@ -1,5 +1,7 @@
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
 
 namespace SmallsOnline.Web.Lib.Services;
 
@@ -7,6 +9,24 @@ public static class OdesliServiceExtensions
 {
     public static IServiceCollection AddOdesliService(this IServiceCollection services, string userAgentName)
     {
+        services.AddHttpClient(
+            name: "OdesliApiClient",
+            configureClient: (serviceProvider, httpClient) =>
+            {
+                httpClient.DefaultRequestHeaders.UserAgent.Add(new(userAgentName, Assembly.GetExecutingAssembly().GetName().Version!.ToString()));
+                httpClient.BaseAddress = new("https://api.song.link/v1-alpha.1/");
+            }
+        );
+
+        services.AddSingleton<IOdesliService, OdesliService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddItunesApiService(this IServiceCollection services, string userAgentName, Action<OdesliServiceOptions> configure)
+    {
+        services.Configure(configure);
+
         services.AddHttpClient(
             name: "OdesliApiClient",
             configureClient: (serviceProvider, httpClient) =>
